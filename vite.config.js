825f15d5517876e89abf253cd6081c2fb43bc8c5
HK560 <lan2496846801@gmail.com>
2025-05-13 22:57:14 +0800

    feat: fix vue-i18n

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-i18n': ['vue-i18n']
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     proxy: {
